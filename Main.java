2713d0848f51f2bd81fc31c4457a429d6af75f79
Enzo Cayetano <>
2021-05-25 03:03:36 +0000

    Updated player input system

--- Main.java
+++ Main.java
@@ -16,21 +16,27 @@ If the room has no items, enter 'none'.
 
 
 // import classes
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 class Main
 {
+
+	public static String InGameName = ""; // instantiate public name variable
+
 	public static void main(String[] args)
 	{
 		InitiateItemsAndRooms();
-		// GetPlayerName();
+		GetPlayerName();
+
+		getPlayerInput("TEST");
 	}
 
 	public static void GetPlayerName()
 	{
-		String InGameName = ""; // instantiate name variable
+		InGameName = JOptionPane.showInputDialog("What is your name, fellow adventurer?");
 
-		InGameName = JOptionPane.showInputDialog("What is your name, adventurer?");
 		if(InGameName == null || InGameName.isEmpty())
 		{
 			InGameName = "Joe"; // default name
@@ -41,9 +47,60 @@ class Main
 	public static void InitiateItemsAndRooms()
 	{
 		Items _PlayerRoom = new Items("rusty knife, cloak of disguise, 50 gold coins");
-		Room PlayerRoom = new Room("Inn of Cavv, Room 1120", "A small, decrepit room with a hard bed, wooden chair, and a dripping ceiling.", _PlayerRoom.items(), "Inn Hallway, S");
+		Room PlayerRoom = new Room("Room 1120", "A small, decrepit room with a hard bed, wooden chair, and a dripping ceiling.", _PlayerRoom.items(), "Inn Hallway, S");
 		PlayerRoom.ParseRooms();
 
-		System.out.println(PlayerRoom.toString());
+		Items _InnHallway = new Items("none");
+		Room InnHallway = new Room("Hallway", "A long hallway that connects all of the tenants'. The floor here is surprisingly kept clean. A door that reads, '" + InGameName + "'", _InnHallway.items(), "Room 1120, N | Stairs, W");
+		InnHallway.ParseRooms();
+	}
+
+	// Get player input
+	public static void getPlayerInput(String question)
+	{
+		// contain answer into String
+		String answer = JOptionPane.showInputDialog(question);
+
+		// make array list and variables that will contain the words
+		ArrayList<String> words = new ArrayList<String>();
+		int startIndex = 0;
+		int endIndex = 0;
+		
+		// parse answer 
+		// iterate through answer to get words
+		for(int i = 0; i < answer.length() - 1; i++)
+		{
+			if(answer.charAt(i + 1) == ' ') // finds space
+			{
+				// set end index
+				endIndex = i + 1;
+
+				// add to words ArrayList
+				words.add(answer.substring(startIndex, endIndex));
+
+				// set start index
+				startIndex = i + 2;
+			}
+			else if(i == answer.length() - 2) // if its the last word
+			{
+				words.add(answer.substring(startIndex));
+			}
+		}
+
+		// analyse
+		String action = words.get(0); // first word is action
+		switch(action)
+		{
+			case "move":
+				MoveRooms();
+			case "look":
+				LookAround();
+			case "inventory":
+				OpenInventory();
+			case "attack":
+				Attack();
+			case "speak":
+				SpeakTo();
+		}
 	}
 } // end of class
